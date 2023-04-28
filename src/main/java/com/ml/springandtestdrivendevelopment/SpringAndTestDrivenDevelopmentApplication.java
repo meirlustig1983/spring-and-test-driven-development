dc3e3710d59b2cba1f7686a235fccc8f85f7f774
meirlustig <meirlustig1983@gmail.com>
2023-04-27 19:14:53 -0700

    first commit

---
+++ src/main/java/com/ml/springandtestdrivendevelopment/SpringAndTestDrivenDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.ml.springandtestdrivendevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndTestDrivenDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndTestDrivenDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ml/springandtestdrivendevelopment/SpringAndTestDrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.springandtestdrivendevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndTestDrivenDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
